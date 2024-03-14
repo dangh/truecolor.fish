6a768e034d3d668583f31b291e94809700f0d305
Dang <huynhminhdang@gmail.com>
2024-03-14 08:22:00 +0800

    format code with fish_indent

--- functions/truecolor.fish
+++ functions/truecolor.fish
@@ -1,23 +1,23 @@
 # from https://unix.stackexchange.com/a/404415
 function truecolor -a width -d "test true color in terminal"
-  if ! string match -q -r '^\\d+$' -- $width
-    if command -q tput
-      set width (command tput cols)
-    else
-      set width 80
+    if ! string match -q -r '^\\d+$' -- $width
+        if command -q tput
+            set width (command tput cols)
+        else
+            set width 80
+        end
     end
-  end
-  awk -v term_cols=$width 'BEGIN{
-    s = "  ";
-    for(colnum = 0; colnum < term_cols; colnum++) {
-      r = 255-(colnum*255/term_cols);
-      g = (colnum*510/term_cols);
-      b = (colnum*255/term_cols);
-      if(g > 255) g = 510-g;
-      printf "\033[48;2;%d;%d;%dm", r,g,b;
-      printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
-      printf "%s\033[0m", substr(s,colnum%2+1,1);
-    }
-    printf "\n";
-  }'
+    awk -v term_cols=$width 'BEGIN{
+        s = "  ";
+        for(colnum = 0; colnum < term_cols; colnum++) {
+            r = 255-(colnum*255/term_cols);
+            g = (colnum*510/term_cols);
+            b = (colnum*255/term_cols);
+            if(g > 255) g = 510-g;
+            printf "\033[48;2;%d;%d;%dm", r,g,b;
+            printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+            printf "%s\033[0m", substr(s,colnum%2+1,1);
+        }
+        printf "\n";
+    }'
 end
